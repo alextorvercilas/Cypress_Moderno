81aa2f73dab31ce56fcb9cad7e2453670e9984a4
Alexander Torvercilas de Melo <alexhg2000@gmail.com>
2024-11-01 08:56:08 -0300

    Initial commit

---
+++ cypress/e2e/arrow.cy.js
@@ -0,0 +1,38 @@
+it('nada agora', function () { })
+
+// function soma(a, b) {
+//     return a + b;
+// }
+// const soma = (a,b)  => a + b
+// console.log (soma(1,7))
+// const soma = function (a, b) {
+//     return a + b
+// }
+
+// const soma = (a, b) => {
+//     return a + b
+// }
+
+// const soma = (a, b) => a + b
+
+// const soma = (a, b) => {
+//     return a + b
+// }
+
+// const soma = (a) => a + a
+
+// const soma = a => a + a
+
+const soma = () => 5 + 5
+
+console.log(soma(1, 4))
+
+it('a function test...', function () {
+    console.log('Function', this)
+})
+
+it('an arrow test...', () => {
+    console.log('Arrow', this)
+})
+
+
